2482986c0517d2d3ca6489c760de4fa40f83d5ba
v4leeria <valeriadonoc@gmail.com>
2024-08-10 22:35:12 -0400

    Arreglo jwt y auth

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -4,6 +4,7 @@ const secret = process.env.JWT_SECRET;
 const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
+  console.log(token);
 
   if (!token) return res.status(401).json({ error: "Token requerido" });
 
